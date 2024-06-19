3aab0c36fe917d2b97433c996cf9f3ecaca7b2cb
leonardo <nickholasleonardo@gmail.com>
2024-06-19 18:29:33 +0700

    feat: connect comment content

--- lib/screen/detail.dart
+++ lib/screen/detail.dart
@@ -74,21 +74,21 @@ class _DetailPageState extends State<DetailPage> {
     }
     bool isReTweet = response['retweet_id'] != null;
     String oriCreator = "";
-      if (isReTweet) {
-        final originalTweetResponse = await Supabase.instance.client
-            .from('tweets')
-            .select('*')
-            .eq('id', response['retweet_id'])
-            .single();
-        final originalCreatorResponse = await Supabase.instance.client
-            .from('user')
-            .select('display_name')
-            .eq('id', originalTweetResponse['creator_id'])
-            .single();
-        oriCreator = originalCreatorResponse['display_name'];
-      } else {
-        final response2 = "";
-      }
+    if (isReTweet) {
+      final originalTweetResponse = await Supabase.instance.client
+          .from('tweets')
+          .select('*')
+          .eq('id', response['retweet_id'])
+          .single();
+      final originalCreatorResponse = await Supabase.instance.client
+          .from('user')
+          .select('display_name')
+          .eq('id', originalTweetResponse['creator_id'])
+          .single();
+      oriCreator = originalCreatorResponse['display_name'];
+    } else {
+      final response2 = "";
+    }
     return Tweet(
         id: response['id'],
         username: userResponse['display_name'],
@@ -127,6 +127,108 @@ class _DetailPageState extends State<DetailPage> {
         oriCreator: "Dummy",
         isRetweetedByUser: false);
   });
+
+  String timeAgo(DateTime timestamp) {
+    DateTime now = DateTime.now();
+    Duration difference = now.difference(timestamp);
+
+    if (difference.inDays >= 365) {
+      int years = (difference.inDays / 365).floor();
+      return "${years}y ago";
+    } else if (difference.inDays >= 30) {
+      int months = (difference.inDays / 30).floor();
+      return "${months}m ago";
+    } else if (difference.inDays >= 7) {
+      int weeks = (difference.inDays / 7).floor();
+      return "${weeks}w ago";
+    } else if (difference.inDays >= 1) {
+      return "${difference.inDays}d ago";
+    } else if (difference.inHours >= 1) {
+      return "${difference.inHours}h ago";
+    } else if (difference.inMinutes >= 1) {
+      return "${difference.inMinutes}m ago";
+    } else {
+      return "${difference.inSeconds}s ago";
+    }
+  }
+
+  Future<List<Tweet>> fetchComments(String id) async {
+    final userId = context.read<SessionBloc>().id ?? "";
+    final response = await Supabase.instance.client
+        .rpc('getcomment', params: {'idtweets': id});
+
+    List<Map<String, dynamic>> data = List<Map<String, dynamic>>.from(response);
+    debugPrint("data ${data.toString()}");
+    List<Tweet> comments = [];
+    final likedTweetsResponse = await Supabase.instance.client
+        .from('likes')
+        .select('tweet_id')
+        .eq('user_id', userId);
+    final likedTweetIds = <String>{};
+
+    for (var record in likedTweetsResponse) {
+      likedTweetIds.add(record['tweet_id']);
+    }
+
+    for (var commentData in data) {
+      DateTime createdAt = DateTime.parse(commentData['created_at']);
+      final userResponse = await Supabase.instance.client
+          .from('user')
+          .select('*')
+          .eq('id', commentData['creator_id'])
+          .single();
+      bool isReTweet = commentData['retweet_id'] != null;
+      String oriCreator = "";
+      if (isReTweet) {
+        final originalTweetResponse = await Supabase.instance.client
+            .from('tweets')
+            .select('*')
+            .eq('id', commentData['retweet_id'])
+            .single();
+        final originalCreatorResponse = await Supabase.instance.client
+            .from('user')
+            .select('display_name')
+            .eq('id', originalTweetResponse['creator_id'])
+            .single();
+        oriCreator = originalCreatorResponse['display_name'];
+      } else {
+        final response2 = "";
+      }
+
+      final retweetCountResponse = await Supabase.instance.client
+          .from('tweets')
+          .select()
+          .eq('retweet_id', commentData['id'])
+          .eq('creator_id', userId);
+
+      int retweetCount = retweetCountResponse.length;
+      bool isRetweetedByUser = false;
+      if (retweetCount > 0) {
+        isRetweetedByUser = true;
+      }
+      comments.add(Tweet(
+          id: commentData['id'],
+          username: userResponse['display_name'],
+          profilePicture: userResponse['display_photo'],
+          verified: false,
+          createdAt: timeAgo(createdAt),
+          content: commentData['content'],
+          media: commentData['media'] != null
+              ? List<String>.from(
+                  commentData['media'].map((item) => item as String))
+              : [],
+          like: commentData['like'],
+          retweet: commentData['retweet'],
+          comment: commentData['comment'],
+          view: 100,
+          isLiked: likedTweetIds.contains(commentData['id']),
+          isReTweet: isReTweet,
+          oriCreator: oriCreator,
+          isRetweetedByUser: isRetweetedByUser));
+    }
+    return comments;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -190,31 +292,55 @@ class _DetailPageState extends State<DetailPage> {
                           }
                         }),
                     Column(
-                      children: tweets
-                          .asMap()
-                          .map((index, tweet) => MapEntry(
-                                index,
-                                index == 0
-                                    ? Padding(
-                                        padding: EdgeInsets.only(top: 10),
-                                        child: Comment(
+                      children: [
+                        FutureBuilder<List<Tweet>>(
+                          future: fetchComments(widget
+                              .id), // Assuming widget.id is the tweet's ID
+                          builder: (context, snapshot) {
+                            if (snapshot.connectionState ==
+                                ConnectionState.waiting) {
+                              return Center(child: CircularProgressIndicator());
+                            } else if (snapshot.hasError) {
+                              return Text("Error: ${snapshot.error}");
+                            } else if (snapshot.hasData &&
+                                snapshot.data!.isNotEmpty) {
+                              print(snapshot);
+                              return Column(
+                                children: snapshot.data!.map((tweet) {
+                                  int index = snapshot.data!.indexOf(tweet);
+                                  return index == 0
+                                      ? Padding(
+                                          padding: EdgeInsets.only(top: 10),
+                                          child: Comment(
+                                            tweet: tweet,
+                                            isBookmarked: Random().nextBool(),
+                                            isLiked: Random().nextBool(),
+                                            isLast: index ==
+                                                snapshot.data!.length - 1,
+                                          ),
+                                        )
+                                      : Comment(
                                           tweet: tweet,
                                           isBookmarked: Random().nextBool(),
                                           isLiked: Random().nextBool(),
-                                          isLast: index == tweets.length - 1,
-                                        ),
-                                      )
-                                    : Comment(
-                                        tweet: tweet,
-                                        isBookmarked: Random().nextBool(),
-                                        isLiked: Random().nextBool(),
-                                        isLast: index ==
-                                            tweets.length -
-                                                1, // Check if the tweet is the last one
-                                      ),
-                              ))
-                          .values
-                          .toList(),
+                                          isLast: index ==
+                                              snapshot.data!.length - 1,
+                                        );
+                                }).toList(),
+                              );
+                            } else {
+                              print("hello");
+                              return Padding(
+                                padding: const EdgeInsets.all(20),
+                                child: Center(
+                                  child: Text("No comments found.",
+                                      textAlign: TextAlign.center),
+                                ),
+                              );
+                            }
+                          },
+                        ),
+                      ],
                     )
                   ],
                 ),
