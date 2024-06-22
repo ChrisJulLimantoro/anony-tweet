84072d80a6d0f41ee656450706ca5521dab7dc4c
leonardo <nickholasleonardo@gmail.com>
2024-06-22 11:48:29 +0700

    fix: overflow on register page

--- lib/screen/register.dart
+++ lib/screen/register.dart
@@ -1,5 +1,4 @@
 import 'dart:math';
-
 import 'package:anony_tweet/main.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -58,94 +57,95 @@ class _RegisterPageState extends State<RegisterPage> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-        // padding: const EdgeInsets.all(20.0),
-        // color: Colors.white,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 100.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    width: 200,
-                    height: 200,
-                    child: Image.asset('lib/assets/logo/Logo.png'),
-                  ),
-                  const SizedBox(height: 16),
-                  Field(
-                    con: usernameController,
-                    isPassword: false,
-                    text: 'Username',
-                    logo: Icons.person,
-                  ),
-                  const SizedBox(height: 16), // Add some spacing between fields
-                  Field(
-                    con: passwordController,
-                    isPassword: true,
-                    text: 'Password',
-                    logo: Icons.lock,
-                  ),
-                  const SizedBox(height: 16),
-                  Field(
-                    con: confirmPasswordController,
-                    isPassword: true,
-                    text: 'Confirm Password',
-                    logo: Icons.lock,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      const Text("Already have an account?"),
-                      TextButton(
-                        onPressed: () {
-                          Navigator.pushReplacementNamed(context, '/login');
-                        },
-                        child: const Text('Log In'),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: ElevatedButton(
-                onPressed: () {
-                  insertData(context);
-                },
-                style: ElevatedButton.styleFrom(
-                    elevation: 0,
-                    backgroundColor: Colors.deepPurple,
-                    textStyle: const TextStyle(
-                      color: Colors.white,
-                    )),
-                child: const Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.login_rounded,
-                        color: Colors.white,
-                      ),
-                      SizedBox(width: 10),
-                      Text(
-                        'SignUp',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 100.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 200,
+                      height: 200,
+                      child: Image.asset('lib/assets/logo/Logo.png'),
+                    ),
+                    const SizedBox(height: 16),
+                    Field(
+                      con: usernameController,
+                      isPassword: false,
+                      text: 'Username',
+                      logo: Icons.person,
+                    ),
+                    const SizedBox(
+                        height: 16), // Add some spacing between fields
+                    Field(
+                      con: passwordController,
+                      isPassword: true,
+                      text: 'Password',
+                      logo: Icons.lock,
+                    ),
+                    const SizedBox(height: 16),
+                    Field(
+                      con: confirmPasswordController,
+                      isPassword: true,
+                      text: 'Confirm Password',
+                      logo: Icons.lock,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        const Text("Already have an account?"),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pushReplacementNamed(context, '/login');
+                          },
+                          child: const Text('Log In'),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    insertData(context);
+                  },
+                  style: ElevatedButton.styleFrom(
+                      elevation: 0,
+                      backgroundColor: Colors.deepPurple,
+                      textStyle: const TextStyle(
+                        color: Colors.white,
+                      )),
+                  child: const Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.login_rounded,
+                          color: Colors.white,
+                        ),
+                        SizedBox(width: 10),
+                        Text(
+                          'SignUp',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
